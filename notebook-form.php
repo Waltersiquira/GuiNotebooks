a0fd0ab449de13f2565c99de93e78bf202f670b1
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-24 09:04:55 -0300

    Create notebook-form.php

---
+++ notebook-form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiNotebooks</title>
+</head>
+<body>
+    <form action="enviar-form-notebook.php" method="post">
+        digite seu nome: <br>
+        <input type="text" name="nome"> <br>
+        digite as especificações: <br>
+        <input type="text" name="especificacoes"> <br>
+        digite um preço: <br>
+        <input type="text" name="preco"> <br>
+        digite uma imagem: <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
