7656896e53ec6fbba0e97da2fa9daa1896bbc573
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-24 08:36:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiNotebooks</title>
+</head>
+<body>
+    <a href="notebook-form.php">adcionar notebook</a>
+    <?php require_once 'includes/lojas.php' ?>
+    <h1>GuiNotebooks</h1>
+    <hr>
+    <table>
+        <?php 
+        $busca = $l->query('select * from notebook');
+        if (!$busca){
+            echo 'erro';
+        } else {
+            if ($busca->num_rows == 0){
+                echo 'erro';
+            } else {
+                while ($reg=$busca->fetch_object()){
+                    echo "<tr><td><a href='pagina-notebook.php?i=$reg->id'><img src='$reg->imagem' width='200'></a><td><td>$reg->nome</td></tr>";
+                }
+            }
+        }
+        ?>
+    </table>
+</body>
+</html>
